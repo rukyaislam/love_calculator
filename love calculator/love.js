61b46a18e156d0a70530974ca7cca81d6318adc8
rukyaislam <72370986+rukyaislam@users.noreply.github.com>
2021-11-20 17:23:46 +0600

    Add files via upload

---
+++ love calculator/love.js
@@ -0,0 +1,15 @@
+
+let first=prompt("FirstName");
+
+let second=prompt("partername");
+
+
+
+let number=Math.random()*100;
+
+number=Math.floor(number)+1;
+document.write ("love percentage"+ number + "%");
+
+// alert
+// (first + "and "+ second + "love percentage "+ number + "%");
+
